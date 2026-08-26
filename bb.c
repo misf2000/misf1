7fd3b70f14d4046ad4ac635f49e86ab553852d30
misf2000 <373868277@qq.com>
2026-08-27 06:21:37 +0800

    the first up

---
+++ bb.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main(void)
+{
+int i,j;
+for(i=1;i<=9;i++){
+
+for(j=1;j<=9;j++){
+if (j<i)
+printf("");
+
+else
+
+printf("%d*%d=%2d\t",i,j,i*j);
+}
+printf("\n");
+
+}
+
+return 0;
+
+}
